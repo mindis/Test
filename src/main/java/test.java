b2f3f9a2968944c66910278f2fd60c54853de921
mindis <mindaugas.zickus@yahoo.com>
2016-04-19 15:47:35 +0100

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 19/04/16
+ * Time: 12:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+    public static void main(String[] args){
+
+        System.out.println("Hello Mindis ");
+        System.out.println("Hello Mindis2 ");
+    }
+}
